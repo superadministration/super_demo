8bafdab3a320993cbc1b054eb273d5ba0132be91
Zach Ahn <engineering@zachahn.com>
2021-12-26 20:34:25 -0500

    Set custom display schema for Orders

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,6 +5,20 @@ class Admin::OrdersController < AdminController
     Order
   end
 
+  def display_schema
+    Super::Display.new do |f, type|
+      f[:id] = type.batch
+      f[:customer_id] = type.real(:record) { |record| record.customer.name }
+      if current_action.index?
+        f[:number_of_order_lines] = type.computed(:record) { |record| record.order_lines.size }
+      else
+        f[:products] = type.computed(:record) { |record| record.products.map(&:name).join(", ") }
+        f[:updated_at] = type.timestamp
+        f[:created_at] = type.timestamp
+      end
+    end
+  end
+
   def form_schema
     customers = Customer.all.map do |c|
       [c.name, c.id]
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_lines, dependent: :destroy
+  has_many :products, through: :order_lines
   accepts_nested_attributes_for :order_lines, reject_if: -> (attrs) { attrs["product_id"].blank? }
 end
