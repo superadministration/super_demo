f2a832b917a52330a495f5c79f22c7c35e4d6389
Zach Ahn <engineering@zachahn.com>
2021-12-26 20:15:05 -0500

    Add redirects to the admin pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
     resources :orders
     resources :customers
     resources :products
+    root to: redirect(path: "/admin/orders", status: 302)
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: redirect(path: "/admin/orders", status: 302)
 end
