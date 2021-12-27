976203ec86dd5553c7b65afb57d87f921a0bca08
Zach Ahn <engineering@zachahn.com>
2021-12-26 20:11:36 -0500

    Set seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+srand 139
+ActiveRecord::Base.transaction do
+  calculator = Product.create!(name: "Graphing calculator", price_cents: 100_00)
+  cheeseburger = Product.create!(name: "Cheeseburger", price_cents: 6_00)
+  magazine = Product.create!(name: "Magazine", price_cents: 3_00)
+  sneakers = Product.create!(name: "Sneakers", price_cents: 55_00)
+  tote = Product.create!(name: "Tote", price_cents: 20_00)
+  products = [calculator, cheeseburger, magazine, sneakers, tote]
+
+  (1..1000).each do |i|
+    puts "Creating Customers #{i} to #{i + 99}" if i % 100 == 1
+    name = %w[Alice Bob Carol Dan][i % 4]
+    Customer.create!(name: "#{name} #{i}")
+  end
+
+  Customer.order(:id).find_each do |customer|
+    puts "Creating Orders for Customers #{customer.id} to #{customer.id + 99}" if customer.id % 100 == 1
+    number_of_orders = rand(1..3)
+    number_of_orders.times do
+      order = customer.orders.build
+      order_products = products.sample(rand(1..products.size))
+      order_products.each do |product|
+        order.order_lines.build(product: product)
+      end
+      order.save!
+    end
+  end
+end
