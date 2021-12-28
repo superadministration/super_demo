5f962421fda46631aaf69243331314959109a761
Zach Ahn <engineering@zachahn.com>
2021-12-27 19:00:09 -0500

    Create fewer records
    This brings the number of records down from ~8k to ~5k.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,16 @@ ActiveRecord::Base.transaction do
   tote = Product.create!(name: "Tote", price_cents: 20_00)
   products = [calculator, cheeseburger, magazine, sneakers, tote]
 
-  (0...1000).each do |i|
+  (0...500).each do |i|
     puts "Creating Customers #{i + 1} to #{i + 100}" if i % 100 == 0
     name = %w[Alice Bob Carol Dan][i % 4]
     Customer.create!(name: "#{name} #{i}")
   end
 
+  numbers_of_orders = [1, 2, 2, 3, 3]
   Customer.order(:id).find_each do |customer|
     puts "Creating Orders for Customers #{customer.id} to #{customer.id + 99}" if customer.id % 100 == 1
-    number_of_orders = rand(1..3)
+    number_of_orders = numbers_of_orders.sample
     number_of_orders.times do
       order = customer.orders.build
       order_products = products.sample(rand(1..products.size))
